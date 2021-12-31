23ea4bb3729311a6f55c24c84cf57c8386b64f0c
Philipp Specht <philippspecht1990@gmail.com>
2021-12-31 16:29:51 +0100

    add mock bookings

---
+++ src/app/bookings/bookings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { Bookings } from '../mock-bookings';
+
+@Component({
+  selector: 'app-bookings',
+  templateUrl: './bookings.component.html',
+  styleUrls: ['./bookings.component.css'],
+})
+export class BookingsComponent implements OnInit {
+  constructor() {}
+
+  bookings = Bookings;
+
+  ngOnInit(): void {}
+}
---
+++ src/app/mock-bookings.ts
@@ -0,0 +1,46 @@
+import { Booking } from './booking';
+
+export const Bookings: Booking[] = [
+  {
+    id: 1,
+    name: 'Philipp Specht',
+    roomNumber: 10,
+    startDate: new Date(),
+    endDate: new Date('2022-01-25'),
+  },
+  {
+    id: 2,
+    name: 'Manuel Neuer',
+    roomNumber: 4,
+    startDate: new Date('2022-01-3'),
+    endDate: new Date('2022-01-10'),
+  },
+  {
+    id: 3,
+    name: 'Tilo Nem',
+    roomNumber: 5,
+    startDate: new Date('2022-01-5'),
+    endDate: new Date('2022-01-12'),
+  },
+  {
+    id: 4,
+    name: 'Sabnrina Nadu',
+    roomNumber: 20,
+    startDate: new Date('2022-01-2'),
+    endDate: new Date('2022-01-11'),
+  },
+  {
+    id: 5,
+    name: 'Ken Saver',
+    roomNumber: 3,
+    startDate: new Date('2022-01-10'),
+    endDate: new Date('2022-01-16'),
+  },
+  {
+    id: 6,
+    name: 'Tina Turner',
+    roomNumber: 23,
+    startDate: new Date('2022-01-20'),
+    endDate: new Date('2022-01-25'),
+  },
+];
